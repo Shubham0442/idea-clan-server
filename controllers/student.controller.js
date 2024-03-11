2006b1a2b052ba9fc25c97565d2a6fc2d11190a6
Shubham Rakhonde <shubhamrakhonde507@gmail.com>
2024-03-11 23:39:12 +0530

    minor updation done

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -22,18 +22,12 @@ studentController.get(
 );
 
 studentController.patch(
-  "/update-courses/:studentId",
-  authentication,
-  authorization(["admin"], async (req, res) => {})
-);
-
-studentController.patch(
   "/update-role/:studentId",
   authentication,
   authorization(["admin"], async (req, res) => {
     try {
       const { studentId } = req.params;
-      const students = await User.find({ _id: studentId }, { ...req.body });
+      await User.findByIdAndUpdate({ _id: studentId }, { ...req.body });
       res.status(200).send({ message: "Updated Role Successfully" });
     } catch (error) {
       res
